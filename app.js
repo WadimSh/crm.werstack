5520681945deb71b84bcf820730ca7f4a180d6b8
WadimSh <wadim11@yandex.ru>
2023-11-13 11:13:19 +0300

    feat: создан сервер на express

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(
+    `<html>
+    <body style='display: flex; align-items: center; justify-content: center;'>
+      <p>crm.werstack</p>
+    </body>
+    </html>`
+  );
+}); 
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+}) 
